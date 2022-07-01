123b08166036758882bfb50355b560f42ae2f9ec
Alyssa Wilson <alyssaw2194@gmail.com>
2022-07-01 00:09:01 -0400

    chardle

---
+++ exercises/ex01.chardle.py
@@ -0,0 +1,34 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__ ="730440563"
+
+
+word: str = input("Enter a five letter word ")
+if len(word) < 5 or len(word) > 5:
+    print("Error: Word must contain 5 characters")
+    exit()
+
+chara: str = input("Enter a single character ")
+if len(chara) < 1 or len(chara) > 1:
+    print("Error: Character must be a single character.")
+    exit()
+
+print("Searching for " + chara + " in " + word)
+int = 0 
+if chara == word[0]:
+    print(chara + " found at index 0") 
+    int += 1
+if chara == word[1]:
+    print(chara + " found at index 1")
+    int += 1
+if chara == word[2]:
+    print(chara + " found at index 2")
+    int += 1
+if chara == word[3]:
+    print(chara + " found at index 3")
+    int += 1
+if chara == word[4]:
+    print(chara + " found at index 4")
+    int += 1
+
+print(str(int) + " instances of " + chara + " found in " + word) 
