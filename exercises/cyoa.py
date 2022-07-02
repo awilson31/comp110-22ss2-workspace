09c6b00e4b8f4ff2f86a1cc4cb8e29d7f0bfe432
Alyssa Wilson <alyssaw2194@gmail.com>
2022-07-01 21:51:02 -0400

    'escape'

--- exercises/cyoa.py
+++ exercises/cyoa.py
@@ -1,4 +1,24 @@
 __author__ = "730440563"
 
-if __name__ == "__main__":
+def main() -> None:
+    randnum = [1, 2, 3, 4, 5]
+    print(random.choice(randnum))
+    points = 0
+#if __name__ == "__main__":
     main()
+def greet() -> None:
+    greet: str = "Welcome to the number guessing game! Your job is to guess a number from 1-5."
+    print greet 
+    player: str = input("What is your name? ")
+    return player
+
+guess: str = input(player + ", guess a number between 1-5: ")
+    if guess > randnum:
+        points += 1
+        print("too high!")
+    if guess < randnum:
+        points += 2
+        print("too low")
+    if guess == randnum:
+        points += 3
+        print("You got the secret number")
